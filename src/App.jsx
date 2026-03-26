1172cbc40c789b145410d54d4c2662ddd3f53231
Adrian Alejandro Montes Mendoza <alejandromontes393@gmail.com>
2026-03-26 11:07:17 -0600

    initial project files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+          <span><i>Logo</i></span>
+        <span><i>Burguer icon</i></span>
+        <nav>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'NutApp',
+        short_name: 'NutApp',
+        description: 'Gestión inteligente de dietas familiares con OCR e IA',
+        theme_color: '#8d7b68',
+        background_color: '#FFFFFF',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }]
+        }
+    })
+  ],
+})
