815975bae52144d6b68ead0839fcb8eea0f086cb
Adrian Alejandro Montes Mendoza <alejandromontes393@gmail.com>
2026-03-30 16:07:43 -0600

    feat: enhance WeeklyDiet component with responsive table wrapper and styling improvements

--- src/views/WeeklyDiet/WeeklyDiet.jsx
+++ src/views/WeeklyDiet/WeeklyDiet.jsx
@@ -5,60 +5,62 @@ export default function WeeklyDiet() {
       <h1>
         <span className={styles.highlight}>Dieta</span> Semanal
       </h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Día/Tiempo</th>
-            <th>Desayuno</th>
-            <th>Colación</th>
-            <th>Comida</th>
-            <th>Colación</th>
-            <th>Cena</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Lunes</td>
-            <td>Huevos con pan</td>
-            <td>Frutas</td>
-            <td>Pollo con arroz</td>
-            <td>Yogur</td>
-            <td>Salmón con verduras</td>
-          </tr>
-          <tr>
-            <td>Martes</td>
-            <td>Avena con leche</td>
-            <td>Nueces</td>
-            <td>Pasta con tomate</td>
-            <td>Manzana</td>
-            <td>Pavo con papas</td>
-          </tr>
-          <tr>
-            <td>Miércoles</td>
-            <td>Tostadas con queso</td>
-            <td>Banana</td>
-            <td>Carne con patatas</td>
-            <td>Almendras</td>
-            <td>Filete con ensalada</td>
-          </tr>
-          <tr>
-            <td>Jueves</td>
-            <td>Pancakes</td>
-            <td>Naranja</td>
-            <td>Atún con verduras</td>
-            <td>Queso fresco</td>
-            <td>Pollo al horno con brócoli</td>
-          </tr>
-          <tr>
-            <td>Viernes</td>
-            <td>Cereal con leche</td>
-            <td>Frutos secos</td>
-            <td>Lentejas con arroz</td>
-            <td>Pera</td>
-            <td>Bacalao con batata</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles.tableWrapper}>
+        <table>
+          <thead>
+            <tr>
+              <th>Día/Tiempo</th>
+              <th>Desayuno</th>
+              <th>Colación</th>
+              <th>Comida</th>
+              <th>Colación</th>
+              <th>Cena</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Lunes</td>
+              <td>Huevos con pan</td>
+              <td>Frutas</td>
+              <td>Pollo con arroz</td>
+              <td>Yogur</td>
+              <td>Salmón con verduras</td>
+            </tr>
+            <tr>
+              <td>Martes</td>
+              <td>Avena con leche</td>
+              <td>Nueces</td>
+              <td>Pasta con tomate</td>
+              <td>Manzana</td>
+              <td>Pavo con papas</td>
+            </tr>
+            <tr>
+              <td>Miércoles</td>
+              <td>Tostadas con queso</td>
+              <td>Banana</td>
+              <td>Carne con patatas</td>
+              <td>Almendras</td>
+              <td>Filete con ensalada</td>
+            </tr>
+            <tr>
+              <td>Jueves</td>
+              <td>Pancakes</td>
+              <td>Naranja</td>
+              <td>Atún con verduras</td>
+              <td>Queso fresco</td>
+              <td>Pollo al horno con brócoli</td>
+            </tr>
+            <tr>
+              <td>Viernes</td>
+              <td>Cereal con leche</td>
+              <td>Frutos secos</td>
+              <td>Lentejas con arroz</td>
+              <td>Pera</td>
+              <td>Bacalao con batata</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </main>
   )
 }
